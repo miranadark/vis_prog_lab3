6b76fb6cbb6eb75f033d19de49a1675ee5956fa5
miranadark <57747648+miranadark@users.noreply.github.com>
2022-11-02 00:21:25 +0700

    Upload lab3

---
+++ lab3/visual_programming_lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace visual_programming_lab2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RomanNumber testr = new RomanNumber(10);
+			Console.WriteLine($"Первое число {testr}");
+			RomanNumber testr2 = new RomanNumber(6);
+			Console.WriteLine($"2 число {testr2}");
+			Console.WriteLine($"Сложение = {testr + testr2}");
+			Console.WriteLine($"Вычитание = {testr - testr2}");
+			Console.WriteLine($"Умножение {testr * testr2}");
+			Console.WriteLine($"Деление {testr / testr2}");
+			Console.WriteLine("");
+			Console.WriteLine($"Изначальное число {testr2.ToString()}");
+			var clon = (RomanNumber)testr2.Clone();
+			Console.WriteLine($"Скопированное число {clon.ToString()}\n");
+			RomanNumber sort1 = new RomanNumber(12);
+			RomanNumber sort2 = new RomanNumber(5);
+			RomanNumber sort3 = new RomanNumber(10);
+			RomanNumber[] mas = { sort1, sort2, sort3 };
+			Console.WriteLine($"Изначальный массив");
+			for (int i = 0; i < 3; i++) Console.WriteLine($"{mas[i].ToString()}");
+			Array.Sort(mas);
+			Console.WriteLine($"Отсортированный массив");
+			for (int i = 0; i < 3; i++) Console.WriteLine($"{mas[i].ToString()}");
+		}
+	}
+}
---
+++ lab3/visual_programming_lab2/RomanNumberException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visual_programming_lab2
+{
+	public class RomanNumberException:Exception
+	{
+        //Overriding the Message property
+        public override string Message
+        {
+            get
+            {
+                return "Ошибка: Число должно быть больше 0!";
+            }
+        }
+    }
+}
